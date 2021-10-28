fdc329c1218f21556f56de62843308e6f98fd897
jeanrvos <velasco.jean@gmail.com>
2021-10-28 21:21:37 +0100

    inclui testes

--- Amazonia.DAL.Tests/UnitTest1.cs
+++ Amazonia.DAL.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using Amazonia.DAL.Entidades;
+using Amazonia.DAL.Repositorios;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Amazonia.DAL.Tests
 {
@@ -6,9 +9,56 @@ namespace Amazonia.DAL.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void DeveCriarUmObjetoDoTipoRepositorioLivros()
         {
+            //Arrange
+            RepositorioLivro repositorio;
 
+            //Act
+            repositorio = new RepositorioLivro();
+
+            //Assert
+            Assert.IsNotNull(repositorio);
+        }
+
+        [TestMethod]
+        public void DeveCriarUmaListaDeLivrosNoObjetoDoTipoRepositorioLivros()
+        {
+            //Arrange
+            RepositorioLivro repositorio;
+            List<Livro> livros;
+            var minElementos = 1;
+
+            //Act
+            repositorio = new RepositorioLivro();
+            livros = repositorio.ObterTodos();
+            var quantidadeLivrosNoRepositorio = livros.Count;
+
+            //Assert
+            Assert.IsNotNull(repositorio);
+            Assert.IsNotNull(livros);
+            Assert.IsTrue(quantidadeLivrosNoRepositorio >= minElementos);
+        }
+
+
+        [TestMethod]
+        public void DeveCriarUmaListaDeLivrosNoObjetoDoTipoRepositorioLivrosComFalha()
+        {
+            //Arrange
+            RepositorioLivro repositorio;
+            List<Livro> livros;
+            var quantidadeElementos = 4;
+
+            //Act
+            repositorio = new RepositorioLivro();
+            livros = repositorio.ObterTodos();
+            var quantidadeLivrosNoRepositorio = livros.Count;
+
+            //Assert
+            Assert.IsNotNull(repositorio);
+            Assert.IsNotNull(livros);
+            //Assert.IsTrue(quantidadeLivrosNoRepositorio == quantidadeElementos);
+            Assert.AreEqual(quantidadeLivrosNoRepositorio, quantidadeElementos);
         }
     }
 }
